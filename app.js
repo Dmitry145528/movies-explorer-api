b922e47ce4315a9414f8b24f9b710821892c5c4d
Dmitry Petrov <Dima.demon.net@mail.ru>
2024-02-06 20:24:53 +0400

    feat: Установить пакеты 2

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
+require('dotenv').config();
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+// const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
+// const router = require('./routes/routes');
+// const { login, createUser } = require('./controllers/users');
+// const auth = require('./middlewares/auth');
+const { errorHandler } = require('./errors/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT, MONGO_URL } = process.env;
 
@@ -7,5 +16,13 @@ const app = express();
 mongoose.connect(MONGO_URL || 'mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(requestLogger); // подключаем логгер запросов
+
+app.use(errorLogger); // подключаем логгер ошибок
+
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT || 3000);
